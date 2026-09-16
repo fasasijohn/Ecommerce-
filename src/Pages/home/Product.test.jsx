7b389e5fb9eefc26ec98e88692f8c641b04c1f1a
IncognitoJohn <155202956+IncognitoJohn@users.noreply.github.com>
2026-09-16 21:56:02 +0100

    #3: Fixed axios issues and deployed

---
+++ src/Pages/home/Product.test.jsx
@@ -0,0 +1,58 @@
+import { it ,expect,describe, vi,beforeEach} from 'vitest';
+import {Product} from "./Product.jsx";
+import {render,screen} from "@testing-library/react";
+import {userEvent} from "@testing-library/user-event/dist/cjs/setup/index.js";
+import axios from "axios";
+vi.mock('axios');
+describe('Product Component',()=>{
+    let product;
+    let loadCart;
+    beforeEach(()=>{
+        product = {
+            "id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            "image": "images/products/athletic-cotton-socks-6-pairs.jpg",
+            "name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            "rating": {
+                "stars": 4.5,
+                "count": 87
+            },
+            "priceCents": 1090,
+            "keywords": ["socks", "sports", "apparel"]
+        };
+        loadCart= vi.fn();
+    })
+
+    it('should display the details correctly',()=> {
+
+
+        render(<Product product={product} loadCart={loadCart}/>);
+        expect(screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument();
+        expect(screen.getByText('$10.90')).toBeInTheDocument();
+        expect(screen.getByTestId ('product-image')).toHaveAttribute("src","/images/products/athletic-cotton-socks-6-pairs.jpg");
+        expect(screen.getByTestId ('product-rating-star')).toHaveAttribute("src",'/images/ratings/rating-45.png');
+        expect(screen.getByText('87')).toBeInTheDocument();
+
+    });
+
+
+   it('should add product to the cart', async ()=>{
+       render(<Product product={product} loadCart={loadCart}/>);
+       const user=userEvent.setup();
+       const addCButton = screen.getByTestId('add-to-cart-button')
+
+      await user.click(addCButton);
+       expect(axios.post).toHaveBeenCalledWith(
+           '/api/cart-items',{
+               productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+               quantity:1
+           }
+       );
+       expect(loadCart).toHaveBeenCalled();
+
+
+   });
+
+
+
+
+})
